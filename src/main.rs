2b013264db8c3f068df8325f8566a852a891d53e
Waaler <oyvin.waaler@gmail.com>
2022-12-05 12:04:45 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -202,30 +202,30 @@ fn day5() {
     //parse stack data
     let mut stacks: Vec<Vec<char>> = Vec::new();
     for line in stacks_data.lines().rev().skip(1) {
-        for i in 0..line.chars().skip(1).count(){
-            if i % 4 == 0 && !line.chars().nth(i + 1).unwrap().is_ascii_whitespace(){
-                if stacks.len() < (i/4) + 1{
+        for i in 0..line.chars().skip(1).count() {
+            if i % 4 == 0 && !line.chars().nth(i + 1).unwrap().is_ascii_whitespace() {
+                if stacks.len() < (i / 4) + 1 {
                     stacks.push(vec![]);
                 }
-                stacks[i / 4].push(line.chars().nth(i+1).unwrap());
+                stacks[i / 4].push(line.chars().nth(i + 1).unwrap());
             }
         }
     }
 
     //parse instructions
     let mut instructions: Vec<Vec<usize>> = vec![vec![]; 3];
-    for line in instruction_data.lines().rev(){
+    for line in instruction_data.lines().rev() {
         let mut count = 0;
         for part in line.split_whitespace() {
-            if is_numeric(part){
+            if is_numeric(part) {
                 instructions[count].push(part.parse::<usize>().unwrap());
-                count += 1;           
+                count += 1;
             }
         }
     }
 
     //compute instructions on data
-    for _ in 0..instructions[0].len(){
+    for _ in 0..instructions[0].len() {
         let iterations = instructions[0].pop().unwrap();
         let from = instructions[1].pop().unwrap() - 1;
         let to = instructions[2].pop().unwrap() - 1;
@@ -252,7 +252,7 @@ fn day5() {
 }
 
 fn is_numeric(str: &str) -> bool {
-    for c in str.chars(){
+    for c in str.chars() {
         if !c.is_numeric() {
             return false;
         }
